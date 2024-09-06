a41a6719760665d9713d18812eab4c9cea00b456
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-09-06 19:18:30 +0530

    Use PathVariable

--- spring-boot-rest/src/main/java/com/jason/spring_boot_rest/jobRestController.java
+++ spring-boot-rest/src/main/java/com/jason/spring_boot_rest/jobRestController.java
@@ -4,10 +4,7 @@ import com.jason.spring_boot_rest.model.JobPost;
 import com.jason.spring_boot_rest.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +17,10 @@ public class jobRestController {
     @GetMapping("jobPosts")
     public List<JobPost> getAllJobs(){
         return service.getAllJobs();
+    }
+
+    @GetMapping("jobPost/{postId}")
+    public JobPost getJob(@PathVariable("postId") int postId){
+        return service.getJob(postId);
     }
 }
