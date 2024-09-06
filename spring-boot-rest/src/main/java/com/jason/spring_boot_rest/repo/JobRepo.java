009f477de1cdeb0684ac95b69c858b433a373418
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-09-06 20:24:18 +0530

    Put and Delete Mapping

--- spring-boot-rest/src/main/java/com/jason/spring_boot_rest/repo/JobRepo.java
+++ spring-boot-rest/src/main/java/com/jason/spring_boot_rest/repo/JobRepo.java
@@ -34,4 +34,23 @@ public class JobRepo {
         }
         return null;
     }
+
+    public void updateJob(JobPost jobPost) {
+        for(JobPost job: jobs){
+            if(job.getPostId()==jobPost.getPostId()){
+                job.setPostProfile(jobPost.getPostProfile());
+                job.setPostDesc(jobPost.getPostDesc());
+                job.setReqExperience(jobPost.getReqExperience());
+                job.setPostTechStack(jobPost.getPostTechStack());
+            }
+        }
+    }
+
+    public void deleteJob(int postId) {
+        for(JobPost job: jobs){
+            if(job.getPostId()==postId){
+                jobs.remove(job);
+            }
+        }
+    }
 }
