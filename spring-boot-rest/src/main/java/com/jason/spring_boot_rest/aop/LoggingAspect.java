9c1ea250705a9819fa16a190f5fa93605d97ab28
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-09-16 00:49:11 +0530

    Use AfterThrowing and AfterReturning

--- spring-boot-rest/src/main/java/com/jason/spring_boot_rest/aop/LoggingAspect.java
+++ spring-boot-rest/src/main/java/com/jason/spring_boot_rest/aop/LoggingAspect.java
@@ -1,9 +1,7 @@
 package com.jason.spring_boot_rest.aop;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -25,5 +23,17 @@ public class LoggingAspect {
             "execution (* com.jason.spring_boot_rest.service.JobService.getAllJobs(..))")
     public void LogMethodExecuted(JoinPoint jp){
         LOGGER.info("method executed " + jp.getSignature().getName());
+    }
+
+    @AfterThrowing("execution (* com.jason.spring_boot_rest.service.JobService.getJob(..)) || " +
+            "execution (* com.jason.spring_boot_rest.service.JobService.getAllJobs(..))")
+    public void LogMethodCrashed(JoinPoint jp){
+        LOGGER.info("method has some issues " + jp.getSignature().getName());
+    }
+
+    @AfterReturning("execution (* com.jason.spring_boot_rest.service.JobService.getJob(..)) || " +
+            "execution (* com.jason.spring_boot_rest.service.JobService.getAllJobs(..))")
+    public void LogMethodExecutedSuccess(JoinPoint jp){
+        LOGGER.info("method Executed Successfully " + jp.getSignature().getName());
     }
 }
