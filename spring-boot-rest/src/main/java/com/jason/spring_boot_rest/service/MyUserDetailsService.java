e18ec3815303bae07e9ddc41c9fda88ba6df9b57
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-10-01 16:42:35 +0530

    Secure Job App

---
+++ spring-boot-rest/src/main/java/com/jason/spring_boot_rest/service/MyUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.jason.spring_boot_rest.service;
+
+
+import com.jason.spring_boot_rest.model.User;
+import com.jason.spring_boot_rest.model.UserPrincipal;
+import com.jason.spring_boot_rest.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repo.findByUsername(username);
+        if(user==null){
+            System.out.println("user 404");
+            throw new UsernameNotFoundException("User 404");
+        }
+        return new UserPrincipal(user);
+    }
+}
